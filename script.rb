7175c19158184d9cfea0ad8a820b19ad043930b9
7minutech <elijohnson1107@gmail.com>
2024-06-28 00:48:13 -0500

    Completed bubble_sort method

--- script.rb
+++ script.rb
@@ -1,9 +1,22 @@
 def bubble_sort(arr)
+    #only need to go through this many times
+    for i in 0...arr.length-1 do
+        #each time we go iterate
+        #need to look through one less element
+        for j in (0...(arr.length-1-i)) do
+            if arr[j] > arr[j+1]
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            end
+        end
+    end
+    arr
 end
 
 
 
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([5, 3, 8, 4, 2])
 #[0,2,2,3,4,78]
 
